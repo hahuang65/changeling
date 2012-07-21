399075bf0dce09da92c6a89f81f14b81ad181536
Howard Huang <hahuang65@gmail.com>
2012-07-20 22:46:37 -0700

    Logling and spec. Basic functionality to keep track of changes on an object.

--- lib/changeling/models/logling.rb
+++ lib/changeling/models/logling.rb
@@ -1,15 +1,35 @@
 module Changeling
   module Models
     class Logling
+      attr_accessor :klass, :object_id, :before, :after
+
       class << self
         def create(object, changes)
           logling = self.new(object, changes)
           logling.save
         end
+
+        def parse_changes(changes)
+          before = {}
+          after = {}
+
+          changes.each_pair do |attr, values|
+            before[attr] = values[0]
+            after[attr] = values[1]
+          end
+
+          [before, after]
+        end
+      end
+
+      def initialize(object, changes)
+        self.klass = object.class.to_s.underscore.pluralize
+        self.object_id = object.id.to_s
+
+        self.before, self.after = Logling.parse_changes(changes)
       end
 
       def save
-
       end
     end
   end
--- spec/lib/changeling/models/logling_spec.rb
+++ spec/lib/changeling/models/logling_spec.rb
@@ -5,13 +5,37 @@ describe Changeling::Models::Logling do
     @klass = Changeling::Models::Logling
   end
 
+  describe ".new" do
+    before(:each) do
+      @blog_post = BlogPost.new
+      @changes = { "public" => [false, true], "content" => ["Something about Changeling", "Content about Changeling"] }
+
+      @before, @after = @klass.parse_changes(@changes)
+
+      @logling = @klass.new(@blog_post, @changes)
+    end
+
+    it "should set klass as the pluralized version of the class name" do
+      @logling.klass.should == @blog_post.class.to_s.underscore.pluralize
+    end
+
+    it "should set object_id as the stringified object's ID" do
+      @logling.object_id.should == @blog_post.id.to_s
+    end
+
+    it "should set before and after based on .parse_changes" do
+      @logling.before.should == @before
+      @logling.after.should == @after
+    end
+  end
+
   describe ".create" do
     before(:each) do
       @blog_post = BlogPost.new
       @changes = { "public" => [false, true], "content" => ["Something about Changeling", "Content about Changeling"] }
       @blog_post.stub(:changes).and_return(@changes)
 
-      @logling = @klass.new
+      @logling = @klass.new(@blog_post, @changes)
       @klass.should_receive(:new).with(@blog_post, @changes).and_return(@logling)
     end
 
@@ -19,6 +43,23 @@ describe Changeling::Models::Logling do
       @logling.should_receive(:save)
 
       @klass.create(@blog_post, @changes)
+    end
+  end
+
+  describe ".parse_changes" do
+    before(:each) do
+      @blog_post = BlogPost.create(:title => "Changeling", :content => "Something about Changeling", :public => false)
+
+      @before = @blog_post.attributes.select { |attr| attr == "content" }
+
+      @blog_post.content = "Content about Changeling"
+
+      @after = @blog_post.attributes.select { |attr| attr == "content" }
+
+    end
+
+    it "should correctly match the before and after states of the object" do
+      @klass.parse_changes(@blog_post.changes).should == [@before, @after]
     end
   end
 end
