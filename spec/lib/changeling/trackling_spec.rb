28d18ff1b8d743feac415f22a4d1fc4c4f146f57
Howard Huang <hahuang65@gmail.com>
2012-07-22 21:39:45 -0700

    Reorganize trackling tests.

--- spec/lib/changeling/trackling_spec.rb
+++ spec/lib/changeling/trackling_spec.rb
@@ -8,25 +8,33 @@ describe Changeling::Trackling do
   models.each_pair do |model, args|
     before(:each) do
       @object = model.new(args[:options])
-      @changes = args[:changes]
     end
 
-    it "should not create a logling when doing the initial save of a new object" do
-      @klass.should_not_receive(:create)
-      @object.save!
-    end
-
-    it "should create a logling with the changed attributes of an object when it is saved" do
-      # Persist object to DB so we can update it.
-      @object.save!
-
-      @klass.should_receive(:create).with(@object, @changes)
-
-      @changes.each_pair do |k, v|
-        @object.send("#{k}=", v[1])
+    describe "callbacks" do
+      before(:each) do
+        @changes = args[:changes]
       end
 
-      @object.save!
+      it "should not create a logling when doing the initial save of a new object" do
+        @klass.should_not_receive(:create)
+        @object.save!
+      end
+
+      it "should create a logling with the changed attributes of an object when it is saved" do
+        # Persist object to DB so we can update it.
+        @object.save!
+
+        @klass.should_receive(:create).with(@object, @changes)
+
+        @changes.each_pair do |k, v|
+          @object.send("#{k}=", v[1])
+        end
+
+        @object.save!
+      end
+    end
+
+    describe "instance methods" do
     end
   end
 end
