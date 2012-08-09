27e35d05dccf026b48f5e40fd8ad6638fbc12847
Howard Huang <hahuang65@gmail.com>
2012-08-09 13:29:18 -0700

    More elegant way of changing Mongoid versions and including test fixtures.

---
+++ spec/fixtures/models/mongoid/2.4.1/blog_post_no_timestamp.rb
@@ -0,0 +1,9 @@
+class BlogPost
+  include Mongoid::Document
+  include Changeling::Trackling
+  include Changeling::Probeling
+
+  field :title
+  field :content
+  field :public, :type => Boolean
+end
