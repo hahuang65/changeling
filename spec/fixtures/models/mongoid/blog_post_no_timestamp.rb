8a6f0e2b7a614160711fefb5a70b976eb60fc8c7
Howard Huang <hahuang65@gmail.com>
2013-02-08 20:43:02 -0800

    Removed old files.

--- spec/fixtures/models/mongoid/blog_post_no_timestamp.rb
+++
@@ -1,9 +0,0 @@
-class BlogPostNoTimestamp
-  include Mongoid::Document
-  include Changeling::Trackling
-  include Changeling::Probeling
-
-  field :title
-  field :content
-  field :public, :type => Boolean
-end
